a82c1b0c2d1987acae2a8b323d03135178c674f2
Tobias Matthaiou <225997+TumTum@users.noreply.github.com>
2026-04-14 16:24:21 +0200

    Neuste node 22 version

--- devenv.nix
+++ devenv.nix
@@ -22,7 +22,7 @@ in {
 
     languages.javascript = {
       enable = lib.mkDefault true;
-      package = lib.mkDefault pkgs.nodejs_20;
+      package = lib.mkDefault pkgs.nodejs_22;
     };
 
     process.manager.implementation = lib.mkDefault "process-compose";
